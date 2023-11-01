2e9957f43e0dd2cf41e8eb7ed0c4d75249a3c182
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-11-01 21:55:56 +0100

    Empty exercise statement

--- concurrency/app/src/main/java/concurrency/BoundedBlockingQueue.java
+++ concurrency/app/src/main/java/concurrency/BoundedBlockingQueue.java
@@ -1,52 +1,21 @@
 package concurrency;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class BoundedBlockingQueue {
-    private final Queue<Integer> queue;
-    private final int capacity;
+
 
     public BoundedBlockingQueue(int capacity) {
-        this.capacity = capacity;
-        this.queue = new LinkedList<>();
+
     }
 
     public void enqueue(int element) throws InterruptedException {
-        synchronized(queue) {
-            while(queue.size() == capacity){
-                queue.wait();
-            }
-            queue.notifyAll();
-            queue.offer(element);
-            System.out.println("Enqueued "
-                    + element
-                    + ". Queue status: "
-                    + queue
-                    + ". Queue size: "
-                    + queue.size());
-        }
+
     }
 
     public int dequeue() throws InterruptedException {
-        int val;
-        synchronized(queue){
-            while(queue.isEmpty())  {
-                queue.wait();
-            }
-            queue.notifyAll();
-            val = queue.poll();
-            System.out.println("Dequeued "
-                    + val
-                    + ". Queue status: "
-                    + queue
-                    + ". Queue size: "
-                    + queue.size());
-        }
-        return val;
+
     }
 
     public int size() {
-        return queue.size();
+
     }
 }
