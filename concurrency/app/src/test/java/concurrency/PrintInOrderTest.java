534e5acad20370f2884b473d11e1d8ab7f7387af
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-11-01 19:51:29 +0100

    Additional test for print in order

--- concurrency/app/src/test/java/concurrency/PrintInOrderTest.java
+++ concurrency/app/src/test/java/concurrency/PrintInOrderTest.java
@@ -70,4 +70,49 @@ class PrintInOrderTest {
         assertEquals("first\nsecond\nthird\n", outputStreamCaptor.toString());
     }
 
+    /**
+     * Input: [2,1,3] means that this class will be passed to three threads.
+     * The first one will call the second() method,
+     * the second one will call the first() method,
+     * and the third one will call the third() method.
+     *
+     */
+    @Test
+    public void shouldPrintInOrderForInput213() throws InterruptedException {
+        PrintInOrder printInOrder = new PrintInOrder();
+        Runnable first = () -> System.out.println("first");
+        Runnable second = () -> System.out.println("second");
+        Runnable third = () -> System.out.println("third");
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                printInOrder.second(second);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                printInOrder.first(first);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            try {
+                printInOrder.third(third);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        Thread.sleep(1000);
+
+        assertEquals("first\nsecond\nthird\n", outputStreamCaptor.toString());
+    }
+
 }
