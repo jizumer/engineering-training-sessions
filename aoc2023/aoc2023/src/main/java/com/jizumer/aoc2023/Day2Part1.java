ebc3fe4c88989895fe0497f9bf6250e74e10d942
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-12-08 17:29:27 +0100

    Loaded input file

--- aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day2Part1.java
+++ aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day2Part1.java
@@ -1,4 +1,14 @@
 package com.jizumer.aoc2023;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Day2Part1 {
+    List<String> loadGames(String s) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(s));
+        return reader.lines().collect(Collectors.toList());
+    }
 }
--- aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day2Part1Test.java
+++ aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day2Part1Test.java
@@ -1,7 +1,19 @@
 package com.jizumer.aoc2023;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day2Part1Test {
+
+    @Test
+    public void shouldLoadTheListOfGamesByFileName() throws FileNotFoundException {
+        assertEquals(100,
+                new Day2Part1()
+                        .loadGames("src/test/resources/day2-input.txt")
+                        .size());
+    }
 
 }
