fa15e2834cab7b57dee9ae28fb1ef0c81b3ab832
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-12-10 22:36:14 +0100

    Test confirming the correct calculation with the new seed generation method

--- aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day5Part2Test.java
+++ aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day5Part2Test.java
@@ -2,17 +2,26 @@ package com.jizumer.aoc2023;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day5Part2Test {
     public static final String SMALL_INPUT_PATH = "src/test/resources/day5-input-small.txt";
 
     @Test
-    public void shouldLoadTheRightNumberOfSeedsFromFile() throws Exception {
+    public void shouldLoadTheRightNumberOfSeedsFromFile() throws IOException {
         assertEquals(27,
                 new Day5Part2()
                         .loadAlmanac(SMALL_INPUT_PATH)
                         .getSeeds().size());
+    }
+
+    @Test
+    public void shouldCalculateTheRightLowestLocationNumber() throws IOException {
+        assertEquals(46, new Day5Part2()
+                .loadAlmanac(SMALL_INPUT_PATH)
+                .calculateLowestLocationPerSeed());
     }
 
 }
