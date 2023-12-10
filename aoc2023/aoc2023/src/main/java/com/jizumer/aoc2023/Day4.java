a42a6cc1eca5e87f0bafd69b7b145f8b859b47cb
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-12-10 01:09:24 +0100

    Removed unused imports

--- aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day4.java
+++ aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day4.java
@@ -3,7 +3,6 @@ package com.jizumer.aoc2023;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Day4 {
     public static int calculateWinningPointsForLine(String line) {
