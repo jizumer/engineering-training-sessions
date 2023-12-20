d6c44bc071c8123bf31d9eb1f5e036353cce8fcd
Juan I. Zumer <juan.zumer@thoughtworks.com>
2023-12-20 23:08:07 +0100

    Find initial tile

--- aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day10.java
+++ aoc2023/aoc2023/src/main/java/com/jizumer/aoc2023/Day10.java
@@ -5,13 +5,31 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Day10 {
-    public int[][] loadMapFromFile() throws FileNotFoundException {
+    public int[][] loadMapFromFile(final String mapFilePath) throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(
-                "src/test/resources/day10-input.txt"));
+                mapFilePath));
 
         return bufferedReader
                 .lines()
                 .map(line -> line.chars().toArray())
                 .toArray(int[][]::new);
+    }
+
+    public int getFurthestNumberOfSteps(int[][] map) {
+        int[] runnerA = findStartingPoint(map);
+        int[] runnerB = runnerA.clone();
+
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public int[] findStartingPoint(int[][] map) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] == (int) 'S') {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new RuntimeException("No starting point found");
     }
 }
--- aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day10Test.java
+++ aoc2023/aoc2023/src/test/java/com/jizumer/aoc2023/Day10Test.java
@@ -3,6 +3,7 @@ package com.jizumer.aoc2023;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +12,31 @@ class Day10Test {
     @Test
     public void shouldLoadMapFromFile() throws FileNotFoundException {
 
-        int[][] map = new Day10().loadMapFromFile();
+        int[][] map = new Day10()
+                .loadMapFromFile("src/test/resources/day10-input.txt");
         assertEquals(140, map.length);
         assertEquals(140, map[0].length);
+    }
+
+    @Test
+    public void shouldReturnFurthestNumberOfStepsForTheSmallInput() throws FileNotFoundException {
+        int[][] map = new Day10()
+                .loadMapFromFile("src/test/resources/day10-input-small.txt");
+        assertEquals(4, new Day10().getFurthestNumberOfSteps(map));
+    }
+
+    @Test
+    public void shouldFindStartingPoint() throws FileNotFoundException {
+        int[][] map = new Day10()
+                .loadMapFromFile("src/test/resources/day10-input-small.txt");
+        assertArrayEquals(new int[]{1, 1}, new Day10().findStartingPoint(map));
+    }
+
+    @Test
+    public void shouldFindStartingPointForTheLargeInput() throws FileNotFoundException {
+        int[][] map = new Day10()
+                .loadMapFromFile("src/test/resources/day10-input.txt");
+        assertArrayEquals(new int[]{62, 61}, new Day10().findStartingPoint(map));
     }
 
 }
