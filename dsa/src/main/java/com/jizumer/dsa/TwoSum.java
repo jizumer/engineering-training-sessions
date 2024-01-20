17eec5b4ac7ae81c64a638fef2d49a2a8a330ccb
jizumer <jizumer@gmail.com>
2024-01-20 20:55:23 +0100

    Simplified version just using a map to keep track of previously found values' indexes

--- dsa/src/main/java/com/jizumer/dsa/TwoSum.java
+++ dsa/src/main/java/com/jizumer/dsa/TwoSum.java
@@ -1,48 +1,22 @@
 package com.jizumer.dsa;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 //1. Two Sum
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexes = new HashMap<>();
 
-        //ordered and with no duplicates set based on nums
-        Set<Integer> orderedNums = new TreeSet<>();
-        for (int num : nums) {
-            orderedNums.add(num);
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (indexes.containsKey(complement))
+                return new int[]{indexes.get(complement), i};
+            indexes.put(nums[i], i);
         }
 
-        for (Integer num : orderedNums) {
-            int complement = target - num;
-            if (complement == num && orderedNums.contains(complement)) {
-                List<Integer> indexes = findUpToTheTwoFirstIndexes(nums, num);
-                if (indexes.size() == 2) {
-                    return new int[]{indexes.get(0), indexes.get(1)};
-                }
-            }
-            if (orderedNums.contains(complement)) {
-                List<Integer> indexes = findUpToTheTwoFirstIndexes(nums, num);
-                return new int[]{indexes.getFirst(), findUpToTheTwoFirstIndexes(nums, complement).getFirst()};
-            }
-        }
-
-        throw new IllegalArgumentException("No solution found");
+        throw new RuntimeException("Solution not found");
     }
 
-    private List<Integer> findUpToTheTwoFirstIndexes(int[] nums, int num) {
-        int count = 0;
-        List<Integer> indexes = new ArrayList<>();
-        for (int i = 0; i < nums.length && count < 2; i++) {
-            if (nums[i] == num) {
-                indexes.add(i);
-                count++;
-            }
-        }
-        return indexes;
-    }
 
 }
