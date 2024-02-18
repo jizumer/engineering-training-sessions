af722a4a24fdc3070baac8863d461b03f3c1b982
Juan Infante Zumer <jizumer@gmail.com>
2024-02-18 12:44:22 +0100

    Improved version using arrays instead of hashmaps

--- dsa/src/main/java/com/jizumer/dsa/Trie.java
+++ dsa/src/main/java/com/jizumer/dsa/Trie.java
@@ -1,94 +1,96 @@
 package com.jizumer.dsa; // 208. Implement Trie (Prefix Tree)
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Trie {
 
-    private final Map<Character, Node> nodes = new HashMap<>();
+    private final Node root = new Node();
 
     public Trie() {
     }
 
     public void insert(String word) {
-
-        if (word.isEmpty()) {
-            return;
-        }
-
-        Node node;
-        if (!nodes.containsKey(word.charAt(0))) {
-            node = new Node();
-            nodes.put(word.charAt(0), node);
-        } else node = nodes.get(word.charAt(0));
-        node.insert(word);
+        root.insert(word);
     }
 
     public boolean search(String word) {
-
-        if (word.isEmpty()) {
-            return false;
-        }
-        Node node = nodes.get(word.charAt(0));
-        if (node == null) {
-            return false;
-        }
-        return node.search(word);
+        return root.search(word);
     }
 
     public boolean startsWith(String prefix) {
-
-        if (prefix.isEmpty()) {
-            return false;
-        }
-        Node node = nodes.get(prefix.charAt(0));
-        if (node == null) {
-            return false;
-        }
-        return node.startsWith(prefix);
+        return root.startsWith(prefix);
     }
 
     static class Node {
-        boolean endOfWord = false;
-        Character letter;
-        Map<Character, Node> suffixes = new HashMap<>();
+        private boolean endOfWord = false;
 
+        private final int R = 26;
+
+        private final Node[] links = new Node[R];
 
         void insert(String word) {
             if (word.isEmpty()) {
                 return;
             }
-            letter = word.charAt(0);
+            char letter = word.charAt(0);
+            if (links[letter - 'a'] == null) {
+                links[letter - 'a'] = new Node();
+            }
             if (word.length() == 1) {
-                endOfWord = true;
+                links[letter - 'a'].endOfWord = true;
                 return;
             }
-            if (suffixes.get(word.charAt(1)) == null) {
-                suffixes.put(word.charAt(1), new Node());
-            }
-            suffixes.get(word.charAt(1)).insert(word.substring(1));
+            links[letter - 'a'].insert(word.substring(1));
         }
 
         boolean search(String word) {
-            if (word.isEmpty()) return this.letter == null;
-            if (this.letter != word.charAt(0)) return false;
-            if (word.length() == 1) return endOfWord;
-
-            Node suffix = suffixes.get(word.charAt(1));
-            if (suffix == null) return false;
-
-            return suffix.search(word.substring(1));
+            if (word.isEmpty()) {
+                return false;
+            }
+            char letter = word.charAt(0);
+            if (links[letter - 'a'] == null) {
+                return false;
+            }
+            if (word.length() == 1) {
+                return links[letter - 'a'].endOfWord;
+            }
+            return links[letter - 'a'].search(word.substring(1));
         }
 
         public boolean startsWith(String prefix) {
-            if (prefix.isEmpty()) return true;
-            if (this.letter == null || this.letter != prefix.charAt(0)) return false;
-            if (prefix.length() == 1) return true;
+            if (prefix.isEmpty()) {
+                return false;
+            }
+            char letter = prefix.charAt(0);
+            if (links[letter - 'a'] == null) {
+                return false;
+            }
+            if (prefix.length() == 1) {
+                return true;
+            }
+            return links[letter - 'a'].startsWith(prefix.substring(1));
+        }
 
-            Node suffix = suffixes.get(prefix.charAt(1));
-            if (suffix == null) return false;
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "EOW=" + endOfWord +
+                    ", links=" + printLinks() +
+                    '}';
+        }
 
-            return suffix.startsWith(prefix.substring(1));
+        private String printLinks() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (int i = 0; i < R; i++) {
+                if (links[i] != null) {
+                    sb.append((char) (i + 'a')).append(", ");
+                }
+            }
+            sb.append("]");
+            return sb.toString();
         }
     }
 }
